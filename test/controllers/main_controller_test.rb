5d328141a30c34ce2aa9d68db1f03fab43ac88b1
Shawn Balestracci <shawn@roshreview.com>
2016-08-16 23:01:30 -0400

    add tests and fix labels

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class MainControllerTest < ActionDispatch::IntegrationTest
+
+  def ivar(var)
+    @controller.instance_variable_get(var)
+  end
+
   test "should get index" do
-    get main_index_url
+    get main_index_url, for_spell: 'this is a test.', for_format: 'this is (excluded) from text'
     assert_response :success
+    assert_equal(ivar(:@for_spell), 'this is a test.')
+    assert_equal(ivar(:@for_format), 'this is (excluded) from text')
+    assert_equal(ivar(:@spell_results), '.tset a si siht')
+    assert_equal(ivar(:@format_results), 'this is from text')
   end
 
 end
